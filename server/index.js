0dfa44fde5479941661ebf36ed37771ba38cfe37
Mickael Contreras <mickaelcontreras508@gmail.com>
2025-01-24 12:02:56 -0800

    feat(api): add POST endpoint to dynamically add new offset programs

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 // * SERVER * //
 
 import express from 'express';
-
+import { flushSync } from 'react-dom';
+import fs from 'fs'
+import offsetPrograms from './offsetPrograms.js'
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +15,28 @@ app.use(express.json());
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Our Carbon Emissions/Offset project server is live!' });
 });
+
+
+
+
+
+// POST endpoint to add new offset programs
+app.post('/api/offset-programs', (req,res)=> {
+  const newProgram = req.body;
+
+  if (!newProgram.name || !newProgram.link || !newProgram.description) {
+    return res.status(400).json({error: 'All fields (name, link, description) are required.'})
+  }
+  // unique ID for new program
+  newProgram.id = offsetPrograms.length + 1;
+  offsetPrograms.push(newProgram);
+
+  // save updated data to file
+  fs.writeFileSync('./offsetPrograms.js', `module.exports = ${JSON.stringify(offsetPrograms, null, 2)};`);
+
+  res.status(201).json(newProgram);
+});
+
 
 // * START SERVER
 app.listen(PORT, () => {
