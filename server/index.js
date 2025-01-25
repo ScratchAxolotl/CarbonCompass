1ebe699503283a92d79dd04b639f2b1551b8fe3f
Amrita Bahadur <amrita10in@yahoo.com>
2025-01-25 13:22:55 -0800

    Fixed a typo on res.locals

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // * ROUTES
 app.post('/api/electricity', electricityController.getEmissions, (req, res) => {
-  res.status(200).json(res.locals.emissionData);
+  res.status(200).json(res.locals.emissionsData);
 });
 
 // * ROUTES
