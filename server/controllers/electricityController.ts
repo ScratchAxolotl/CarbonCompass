5e334493e9e7f5c97ae225a9a5f91ab1b07a32bc
Mickael Contreras <mickaelcontreras508@gmail.com>
2025-01-30 21:38:08 -0800

    feat: add TypeScript to electricity controller and implement basic error handling
    - Added TypeScript interfaces for request () and response data () in the electricity controller.
    - Implemented validation for required fields in  to handle missing data cases.
    - Updated  function to use TypeScript with strong type safety.

--- server/controllers/electricityController.ts
+++ server/controllers/electricityController.ts
@@ -1,9 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
-const electricityController = {};
 
-electricityController.getEmissions = async (req, res, next) => {
+// const electricityController = {};
+const electricityController: { [key: string]: any } = {}
+
+
+interface ElectricityRequestBody{
+  type: string;
+  electricity_unit: string; 
+  electricity_value: number;
+  country: string;
+  state: string;  
+}
+
+interface ElectricityResponseBody{
+  carbon_lb: number;
+  carbon_kg: number;
+}
+
+
+
+electricityController.getEmissions = async (
+  req: Request<{}, {}, ElectricityRequestBody>,
+    res: Response,
+     next: NextFunction ) => {
   try {
     const {type, country, state, electricity_unit, electricity_value} = req.body;
+
+    if (!type || !country || !state || !electricity_unit || !electricity_value) {
+      return res.status(400).json({message: "Missing required fields in request body"})
+    }
+
     const response = await axios.post(
       'https://www.carboninterface.com/api/v1/estimates',
       {type, electricity_unit, electricity_value, country, state},
@@ -14,10 +41,13 @@ electricityController.getEmissions = async (req, res, next) => {
         }
       }
     );
+
     const carbon_lb = response.data.data.attributes.carbon_lb;
     const carbon_kg = response.data.data.attributes.carbon_kg;
-    const emissionsData = {carbon_lb, carbon_kg};
-    console.log('emissionsData', emissionsData);
+    const emissionsData = { carbon_lb, carbon_kg };
+
+    console.log('Electricity emissionsData', emissionsData);
+
     res.locals.emissionsData = emissionsData;
     return next();
   } catch (error) {
