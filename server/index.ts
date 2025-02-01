4c9f0f0240e92eb06e55f89c2afd1ae05df3a94c
Jeremy Dalton <jeremydalton91@gmail.com>
2025-02-01 18:11:37 -0500

    cleaned up server file to remove redundent imports that are now in the Router

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,7 @@
 // * SERVER * //
 import express from 'express';
-import fs from 'fs';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import offsetPrograms from './offsetPrograms.ts';
 
 // * INITIALIZE .ENV FILE
 dotenv.config();
@@ -12,11 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 import apiRouter from './routers/apiRouter.ts';
-// import electricityController from './controllers/electricityController.ts';
-// import vehicleController from './controllers/vehicleController.ts';
-// import openAiController from './controllers/openAiController.ts';
 
-// * MIDDLEWARE
 
 // * CORS
 app.use(
@@ -29,35 +23,8 @@ app.use(
 
 app.use(express.json());
 
+// * ROUTER
 app.use('/api', apiRouter);
-
-// // * ROUTE - ELECTRICITY
-// app.post('/api/electricity', electricityController.getEmissions, (req, res) => {
-//   res.status(200).json(res.locals.emissionsData);
-// });
-
-// // * ROUTE - VEHICLES
-// // on input form page load, get the vehicle makes for client selection (complete)
-// app.get('/api/vehicle/makes', vehicleController.getMakes, (req, res) => {
-//   res.status(200).json(res.locals.vehicleMakes);
-// });
-// // on make selection, get the vehicle makes for client selection (complete)
-// app.get(
-//   '/api/vehicle/makes/:makeId',
-//   vehicleController.getModels,
-//   (req, res) => {
-//     res.status(200).json(res.locals.vehicleModels);
-//   }
-// );
-// // *TODO the client will send a request on submit to get the emissions data from the form (incomplete)
-// app.post('/api/vehicle', vehicleController.getEmissions, (req, res) => {
-//   res.status(200).json(res.locals.vehicleModels);
-// });
-
-// // * ROUTE - OPENAI
-// app.post('/api/openai/', openAiController.generateResponse, (req, res) => {
-//   res.status(200).json({ aiResponse: res.locals.aiResponse });
-// });
 
 // * START SERVER
 app.listen(PORT, () => {
